822f0328f004de206fffb5e76098146e3a692be6
Alex Miranda <alexandermichaelmiranda@gmail.com>
2025-05-10 10:27:51 -0500

    correcting module pathing

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 # VPC and Network Configuration
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws/modules/vpc"
 
   name = "tree-service-vpc"
   cidr = "10.0.0.0/16"
