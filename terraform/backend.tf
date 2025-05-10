e34a408ffd82c93a3abafa856b1156b6a00c3865
Alex Miranda <alexandermichaelmiranda@gmail.com>
2025-05-10 10:24:03 -0500

    fixing terraform action and backend config to use correct region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tree-service-terraform-state"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-2"
     encrypt        = true
     dynamodb_table = "tree-service-terraform-locks"
   }
